200661beb95aa770724fed5433c496d6d3fb2e11
Martin <mcbride.martin@gmail.com>
2022-07-01 20:53:05 +0100

    Fix test

--- test/test_public.py
+++ test/test_public.py
@@ -1,18 +1,31 @@
 import unittest
 import filecmp
-import os
+
+"""
+Running pybooksite on the sample site creates a collection of all the files needed for deployment, in the public folder.
+
+There is also a set of known good files in the reference-public folder.
+
+This test does a full recursive comapre of every file in every subfolder of these two folders. This is a quick way to 
+check that nothing has changed.
+
+It is necessary to run pybooksite to recreate all the files in the public area before executing this test.
+
+If the code changes in such a way that the outpiut files are expected to have changed, you should check that the new
+files are correct, then update the reference files in the reference-public folder, and check them in.
+"""
 
 _errors = []
 
 def _report_recursive(dcmp):
     for name in dcmp.diff_files:
-        s = "DIFF file %s found in %s and %s".format(name, dcmp.left, dcmp.right)
+        s = "DIFF file {} found in {} and {}".format(name, dcmp.left, dcmp.right)
         _errors.append(s)
     for name in dcmp.left_only:
-        s = "ONLY LEFT file %s found in %s".format(name, dcmp.left)
+        s = "ONLY LEFT file {} found in {}".format(name, dcmp.left)
         _errors.append(s)
     for name in dcmp.right_only:
-        s = "ONLY RIGHT file %s found in %s".format(name, cmp.right)
+        s = "ONLY RIGHT file {} found in {}".format(name, dcmp.right)
         _errors.append(s)
     for sub_dcmp in dcmp.subdirs.values():
         _report_recursive(sub_dcmp)
@@ -24,3 +37,5 @@ class TestPublic(unittest.TestCase):
         _report_recursive(c)
         for e in _errors:
             print(e)
+        self.assertEquals([], _errors)
+
