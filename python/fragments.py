ceb43252d1487a96271b22dcf32ea14151cd36d7
Martin <mcbride.martin@gmail.com>
2022-07-16 22:09:07 +0100

    Clean up fragments code

--- python/fragments.py
+++ python/fragments.py
@@ -6,6 +6,12 @@ import logging
 import os
 
 def get_fragments(config):
+    """
+    Go to fragments directory of current theme. For each file, read the content and store it in the
+    fragments dictionary using the filename as a key.
+    :param config: Config dictionary
+    :return: Dictionary of fragment contents
+    """
 
     fragments_dict = dict()
     theme = config["theme"]
@@ -20,6 +26,26 @@ def get_fragments(config):
     return fragments_dict
 
 def replace_fragments(fragments_dict, dynamic_config):
+    """
+    Replaces fragment place holders in dynamic config with config content.
+
+    For example:
+
+    * fragments_dict contains two entries:
+        * ad1.html: <content of ad1.html file>
+        * ad2.html: <content of ad2.html file>
+
+    * dynamic_config contains __advert: ad1.html (which comes from page yaml)
+
+    * This code will update dynamic config with __advert: <content of ad1.html file>
+
+    * Theme template wil include {{__advert}} that will then be replaced with <content of ad1.html file> when
+      pystache runs.
+
+    :param fragments_dict: Dictionary containing fragments
+    :param dynamic_config: Dynamic page config dictionary
+    :return:
+    """
 
     for key in list(dynamic_config.keys()):
         if key.startswith("__"):
